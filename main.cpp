69ed205aaca4040a07967b962beb8de2d135cd50
Lejing Huang <ohaluminum@users.noreply.github.com>
2020-09-12 00:33:47 -0500

    Final updated.

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@ int main(int argc, char* argv[])
     ArgumentManager am(argc, argv);
 
     //Get the filename of argument name "input" and "output"
-    //string input = am.get("input");
-    //string output = am.get("output");
+    string input = am.get("input");
+    string output = am.get("output");
 
     //Test
-    string input = "input1.txt";
-    string output = "output1.txt";
+    //string input = "input1.txt";
+    //string output = "output1.txt";
 
     ifstream inFS;
     ofstream outFS;
@@ -22,51 +22,52 @@ int main(int argc, char* argv[])
     inFS.open(input);
     outFS.open(output);
 
-    int operation;
+    int operation = 0;
     char A;
     char B;
-    int result;
+    int result = 0;
 
     try
     {
         if (!inFS.is_open())
         {
-            throw runtime_error("File does not exist.");
+            throw runtime_error("ERROR: File not found");
         }
 
         if (inFS.peek() == EOF)
         {
-            throw runtime_error("File is empty.");
+            throw runtime_error("ERROR: File is empty");
         }
-        
 
+        //Read from input file
+        inFS >> operation >> A >> B;
+
+        if (operation == 1)
+        {
+            result = (int)A + (int)B;
+        }
+        else if (operation == 2)
+        {
+            result = (int)A - (int)B;
+        }
+
+        if (result < 0)
+        {
+            throw runtime_error("ERROR: negative result");
+        }
+        else
+        {
+            outFS << result;
+        }
     }
     catch (runtime_error & e)
     {
         outFS << e.what() << endl;
     }
 
-
-    //Read from input file
-    inFS >> operation >> A >> B;
-
-    if (operation == 1)
-    {
-        result = (int)A + (int)B;
-    }
-    else if (operation == 2)
-    {
-        result = (int)A - (int)B;
-    }
-
-    if (result > 0)
-    {
-        outFS << result;
-    }
-    else
-    {
-
-    }
+    //Close all files
+    inFS.close();
+    outFS.close();
 
     return 0;
 }
